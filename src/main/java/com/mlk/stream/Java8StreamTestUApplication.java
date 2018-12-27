65ba30612a271a0d77fe0e8ea5c72f9e414a0c86
SaadiMelek <saadi.melek@gmail.com>
2018-12-27 12:06:22 +0100

    add Product entity & ProductRepository & Insert entries for test

--- src/main/java/com/mlk/stream/Java8StreamTestUApplication.java
+++ src/main/java/com/mlk/stream/Java8StreamTestUApplication.java
@@ -1,14 +1,33 @@
 package com.mlk.stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mlk.stream.work.Product;
+import com.mlk.stream.work.ProductRepository;
+
 @SpringBootApplication
-public class Java8StreamTestUApplication {
+public class Java8StreamTestUApplication implements CommandLineRunner {
+
+	@Autowired
+	ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java8StreamTestUApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
+		// Adding Products
+		productRepository.save(new Product(1, "HP Laptop", 25000f));
+		productRepository.save(new Product(2, "Dell Laptop", 30000f));
+		productRepository.save(new Product(3, "Lenevo Laptop", 28000f));
+		productRepository.save(new Product(4, "Sony Laptop", 28000f));
+		productRepository.save(new Product(5, "Apple Laptop", 90000f));
 
+		productRepository.findAll().stream().forEach(p -> System.out.println(p));
+	}
+
+}
---
+++ src/main/java/com/mlk/stream/work/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.mlk.stream.work;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+}
